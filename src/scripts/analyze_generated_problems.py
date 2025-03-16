0210b84169ef510c2052ae3578eeddfd371f549f
TheAeryan <ccaarlos@ugr.es>
2025-03-16 23:42:05 +0100

    Implementing script for analyzing generated problems #2 - Solved bugs

--- src/scripts/analyze_generated_problems.py
+++ src/scripts/analyze_generated_problems.py
@@ -13,11 +13,11 @@ The specific analysis depends on the command-line argument passed:
 
 >>> Examples of use:
     - NeSIG (experiment identified by parameters):
-        python analyze_generated_problems.py --domain blocksworld --init-policy PPO --goal-policy PPO --seed 0 --max-init-actions-test 15 --max-goal-actions-test 75
+        python analyze_generated_problems.py --domain blocksworld --init-policy PPO --goal-policy PPO --seed 1 --max-init-actions-test 15 --max-goal-actions-test 60
     - NeSIG (experiment identified by experiment_id):
-        python analyze_generated_problems.py --experiment-id 0ae0837594 --max-init-actions-test 15 --max-goal-actions-test 75
+        python analyze_generated_problems.py --experiment-id edf5246679 --init-policy PPO --goal-policy PPO --max-init-actions-test 15 --max-goal-actions-test 60
     - adhoc:
-        python analyze_generated_problems.py --experiment-id 8-10_3-10__1_100_1.0 --domain blocksworld
+        python analyze_generated_problems.py --init-policy adhoc --goal-policy adhoc --domain blocksworld --experiment-id 8-10_3-10__1_100_1.0 
 """
 
 import argparse
@@ -32,18 +32,23 @@ def load_json(file_path : Path):
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('base_folder_nesig', type=str, default='../../experiments', help='The path to the folder containing all the NeSIG experiments')
-    parser.add_argument('base_folder_adhoc', type=str, default='../../data/instance_generators', help='The path to the folder containing all the ad hoc experiments')
+    parser.add_argument('--base_folder_nesig', type=str, default='../../experiments', help='The path to the folder containing all the NeSIG experiments')
+    parser.add_argument('--base_folder_adhoc', type=str, default='../../data/instance_generators', help='The path to the folder containing all the ad hoc experiments')
     parser.add_argument('--experiment-id', type=str, default=None, help='The id of the experiment to analyze')
     parser.add_argument('--domain', type=str, default=None, help='The domain of the experiment')
-    parser.add_argument('--init-policy', type=str, default=None, help='The init-policy of the experiment: PPO, random or adhoc')
-    parser.add_argument('--goal-policy', type=str, default=None, help='The goal-policy of the experiment: PPO, random or adhoc')
+    parser.add_argument('--init-policy', type=str, required=True, help='The init-policy of the experiment: PPO, random or adhoc')
+    parser.add_argument('--goal-policy', type=str, required=True, help='The goal-policy of the experiment: PPO, random or adhoc')
     parser.add_argument('--seed', type=int, default=None, help='The seed of the experiment')
-    parser.add_argument('--max-init-actions-test', type=int, required=True, help='Used to differentiate between the different test problem sizes of the same experiment')
-    parser.add_argument('--max-goal-actions-test', type=int, required=True, help='Used to differentiate between the different test problem sizes of the same experiment')
+    parser.add_argument('--max-init-actions-test', type=int, default=None, help='Used to differentiate between the different test problem sizes of the same experiment')
+    parser.add_argument('--max-goal-actions-test', type=int, default=None, help='Used to differentiate between the different test problem sizes of the same experiment')
     parser.add_argument('--get-unique-problems', action='store_true', help='Get the unique problems in the experiment')
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    args.base_folder_nesig = Path(args.base_folder_nesig)
+    args.base_folder_adhoc = Path(args.base_folder_adhoc)
+
+    return args
 
 def get_problems_adhoc(args):
     """
@@ -67,7 +72,8 @@ def get_problems_nesig(args):
     In order to find the experiment, it will iterate over all experiments in the base_folder_nesig and check if the arguments match.
     """
     assert (args.experiment_id is None) != (args.domain is None), 'For NeSIG, the experiment must be identified by either its parameters or its experiment_id but not both' 
-    
+    assert args.max_init_actions_test is not None and args.max_goal_actions_test is not None, 'For NeSIG, the test problem size must be given'
+
     matched_experiments_id = []
 
     for experiment_folder in args.base_folder_nesig.glob('*'):
@@ -87,10 +93,13 @@ def get_problems_nesig(args):
 
             if experiment_info['domain'] == args.domain and experiment_info['init_policy'] == args.init_policy and \
                experiment_info['goal_policy'] == args.goal_policy and experiment_info['seed'] == args.seed:
-                print(f"> Matched experiment with id {experiment_folder.name}")
-                matched_experiments_id.append(experiment_folder.name)
-
-                problems_info = load_json(experiment_folder / 'test' / f'{args.max_init_actions_test}_{args.max_goal_actions_test}' / 'results.json')
+                
+                try:         
+                    problems_info = load_json(experiment_folder / 'test' / f'{args.max_init_actions_test}_{args.max_goal_actions_test}' / 'results.json')           
+                    print(f"> Matched experiment with id {experiment_folder.name}")
+                    matched_experiments_id.append(experiment_folder.name)
+                except FileNotFoundError:
+                    continue
 
     assert len(matched_experiments_id) == 1, f"Error: found {len(matched_experiments_id)} experiments with the given parameters: {matched_experiments_id}"
 
